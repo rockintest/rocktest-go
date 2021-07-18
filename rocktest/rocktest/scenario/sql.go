f8e2a3f6db1e1eca98b6c2b17c03ff4e71fe6780
Benoit Touron <benoit.touron@gmail.com>
2021-07-18 23:50:50 +0200

    Start SQL module

---
+++ rocktest/rocktest/scenario/sql.go
@@ -0,0 +1,21 @@
+package scenario
+
+func (module *Module) Sql_connect(params map[string]interface{}, scenario *Scenario) error {
+	/*
+		paramsEx := scenario.ExpandMap(params)
+
+		driver, err := scenario.GetString(params, "driver", nil)
+
+		if err != nil {
+			return err
+		}
+
+		url, err := scenario.GetString(params, "url", nil)
+
+		if err != nil {
+			return err
+		}
+	*/
+	return nil
+
+}
