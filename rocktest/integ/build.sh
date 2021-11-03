6d1b90e5cd15b9d6949a5b1613d17c38eefac8d8
Benoit Touron <benoit.touron@thermofisher.com>
2021-11-03 21:47:57 +0100

    build docker

--- rocktest/integ/build.sh
+++ rocktest/integ/build.sh
@@ -8,7 +8,7 @@ test -z "$1" && {
 }
 
 echo "Use release $VER : https://github.com/rockintest/rocktest-go/releases/download/$VER/rocktest-go-$VER-linux-amd64.tar.gz"
-curl https://github.com/rockintest/rocktest-go/releases/download/$VER/rocktest-go-$VER-linux-amd64.tar.gz | tar xzf -
+curl -L https://github.com/rockintest/rocktest-go/releases/download/$VER/rocktest-go-$VER-linux-amd64.tar.gz | tar xzf -
 
 docker build -t rocktest-go .
 
